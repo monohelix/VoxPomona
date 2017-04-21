12c5d3a0dcb908aa6e615e9354149642607657ee
Jerry Yan <Jerry@Jerry-Yan.local>
2017-04-21 12:02:19 -0700

    FUCK

--- VoxPomona/urls.py
+++ VoxPomona/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
 
     #User-Related URLs
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
