31982d171566a9fe3201eec303850583459873ad
Jerry Yan <Jerry@Jerry-Yan.local>
2017-04-21 12:13:56 -0700

    FUCK

--- CS133Project/settings.py
+++ CS133Project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!n^4qqds1#6_z@bsrler065h-5534a%ino*6aens=rn*=fs-(s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
 
