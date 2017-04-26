16b786d953e88fc1d26c0fcb354bf591db7ac4b0
lehuu711 <hil02013@mymail.pomona.edu>
2017-04-26 16:14:10 -0700

    organized/commented urls.py

--- VoxPomona/urls.py
+++ VoxPomona/urls.py
@@ -6,21 +6,29 @@ from . import views
 urlpatterns = [
 	url(r'^$', RedirectView.as_view(pattern_name='home', permanent=False)),
 
-    #User-Related URLs
+    # user-releated urls
     url(r'^login', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^register', views.register_view, name='register'),
     url(r'^logout', views.logout_view, name='logout'),
     url(r'^profile', views.user_profile, name='user_profile'),
+
+    # general purpose urls
     url(r'^home', views.home, name='home'),
-    url(r'^new_petition', views.new_petition_view, name='new_petition'),
-    url(r'^view_petition/([0-9]+)/$', views.view_petition_view, name='view_petition'),
     url(r'^petition/([0-9]+)/$', views.display_petition),
     url(r'^search', views.search_results),
+
+    # petition/clause modification url
+    url(r'^new_petition', views.new_petition_view, name='new_petition'),
+    url(r'^view_petition/([0-9]+)/$', views.view_petition_view, name='view_petition'),
+    url(r'^finalize_petition', views.finalize_petition),
     url(r'^delete_clause', views.delete_clause),
-    url(r'^add_comment', views.add_comment),
-    url(r'^delete_comment', views.delete_comment),
+
+    # change-related urls
     url(r'^add_change', views.add_change),
     url(r'^accept_change', views.accept_change),
     url(r'^reject_change', views.reject_change),
-    url(r'^finalize_petition', views.finalize_petition),
+
+    # comment-related urls
+    url(r'^add_comment', views.add_comment),
+    url(r'^delete_comment', views.delete_comment),
 ]
