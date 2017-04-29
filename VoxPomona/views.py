3db323c0f3a70497a27a1b2f7f79bcab757f19f8
lehuu711 <hil02013@mymail.pomona.edu>
2017-04-28 22:37:36 -0700

    make sure that owner doenst get an email when they sign own petition

--- VoxPomona/views.py
+++ VoxPomona/views.py
@@ -677,8 +677,8 @@ def email_notification(this_petition,messageType):
 
     # owner specific email messages
 
-    # 'S' = a new person has signed
-    if messageType == 'S':
+    # 'S' = a new person has signed, make sure it's not owner
+    if messageType == 'S' && len(signs) > 0:
         latest_sign = signs[0]
         sign_count = signs.count()
         signator = latest_sign.userID
