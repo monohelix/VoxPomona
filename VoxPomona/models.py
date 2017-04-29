888c2bd378b112afb257b16561d18ba101ce22c0
Andi Chen <konstantinchen@gmail.com>
2017-04-29 16:08:00 -0700

    commit

--- VoxPomona/models.py
+++ VoxPomona/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
@@ -149,9 +150,9 @@ class Petition(models.Model):
         return True
 
     def __unicode__(self):
-        return ("petition"+str(self.petitionID)+self.title.encode('utf-8'))
+        return ("petition"+str(self.petitionID)+self.title.encode('utf8'))
     def __str__(self):
-        return ("petition"+str(self.petitionID)+self.title.encode('utf-8'))
+        return ("petition"+str(self.petitionID)+self.title.encode('utf8'))
 
 class Clause(models.Model):
     clauseID = models.AutoField(primary_key=True)
